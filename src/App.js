731f76e0d36a8449638c0e74c0db8d5538d2cc19
prajwalzingare <74585612+prajwalzingare@users.noreply.github.com>
2022-04-17 10:09:30 +0530

    first commite

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <NavBar />
-        <News pageSize={6} country="in" category="technology" />
+        <News pageSize={6} country="in" category="science" />
       </div>
     );
   }
